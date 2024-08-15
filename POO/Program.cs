18e04a4098a8eb3f33278d8b7f57511435e0a6f4
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-08-15 11:13:45 -0300

    Exemplo do uso da propriedade 'Abstração'

---
+++ POO/Program.cs
@@ -0,0 +1,33 @@
+﻿
+/* Programaçao Orientada à Objetos - POO
+ * 
+ *    O principal conceito da POO são classes e objetos
+ *    --> A classe é a representação de um objeto
+ *    --> O Objeto é a classe concretizada, implementada
+ *
+ *    Exemplo:
+ *    Classe --> Pessoa, atributos (Nome, Idade), métodos (apresentar)
+ *    Objeto --> Felipe
+ *
+ * Paradigmas de Programação
+ * 
+ * É um conjunto de regras, modelo de técnicas, estruturas e formas de solucionar um problema
+ * 
+ *  --> Paradigma de programação != Linguagem de programação
+ *  --> Uma linguagem de programação implementa um ou mais paradigmas
+ *  
+ *  Exemplos:
+ *  - Programação orientada à objetos
+ *  - Programação estruturada
+ *  - Programação imperativa
+ *  - Programação procedural
+ *  - Programação orientada à eventos
+ *  - Programação lógica
+ *  - Etc...
+ * 
+ */
+
+using POO.Abstracao;
+
+// 1. Abstração
+Abstracao.ExemploAbstracao();
