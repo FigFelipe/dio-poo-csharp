2e24eed2e93b913f6f5a48b468af36383a249189
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-08-15 11:14:07 -0300

    Create Abstracao.cs

---
+++ POO/Abstracao/Abstracao.cs
@@ -0,0 +1,31 @@
+﻿using POO.Abstracao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Abstracao
+{
+    internal class Abstracao
+    {
+        /* Pilar POO 1. Abstração
+         * 
+         * Abtrair um objeto do mundo real para um contexto específico,
+         * considerando apenas os atributos importantes
+         * 
+         */
+
+        public static void ExemploAbstracao()
+        {
+            // Criando um novo objeto do tipo 'Pessoa'
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Felipe";
+            p1.Idade = 99;
+
+            p1.Apresentar();
+        }
+        
+    }
+}
